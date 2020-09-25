8056de7848f6a906204eda9ac40bddf935a71388
dima4553 <laik111@mail.ru>
2020-09-25 15:46:36 +0300

    fix pr1 pr4 ex1

---
+++ Praktika 1/Практикум 4/Converting/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 i16 = 1;
+            Int32 i32 = 1;
+            double db = 1;
+
+            i16 = 132;
+            i16 = db;
+
+            i32 = i16;
+            i32 = db;
+
+            db = 116;
+            db = i32;
+
+      
+        }
+    }
+}
