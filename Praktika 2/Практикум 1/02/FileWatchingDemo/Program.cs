e91e6bb456cc4778c046f8a122000d3020d8f933
Дмитрий Пинчук <laik111@mail.ru>
2020-11-07 01:41:02 +0300

    add pr2 pr1 ex2

---
+++ Praktika 2/Практикум 1/02/FileWatchingDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatchingDemo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
+            watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+        }
+
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+    }
+}
