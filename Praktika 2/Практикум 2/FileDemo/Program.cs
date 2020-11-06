93e75838545f56d2980f8892b225c62ece5f70db
Дмитрий Пинчук <laik111@mail.ru>
2020-11-07 01:57:03 +0300

    add pr2 pr2

---
+++ Praktika 2/Практикум 2/FileDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer =
+                File.CreateText(
+                    @"C:\Users\laik1\source\repos\dima4553\NewRepo\Praktika 2\Практикум 2\FileDemo\newfile.txt");
+            writer.WriteLine("This is my file");
+            writer.WriteLine("Do you like its format?");
+            writer.Close();
+
+            StreamReader reader =
+                File.OpenText(@"C:\Users\laik1\source\repos\dima4553\NewRepo\Praktika 2\Практикум 2\FileDemo\newfile.txt");
+            string contents = reader.ReadToEnd();
+            reader.Close();
+            Console.WriteLine(contents);
+        }
+    }
+}
