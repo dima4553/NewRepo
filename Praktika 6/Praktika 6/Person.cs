ba61d546c27abc407ddb0cc17287c0b3c64a667a
Дмитрий Пинчук <laik111@mail.ru>
2020-11-17 22:46:13 +0300

    add pr6

---
+++ Praktika 6/Praktika 6/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Praktika_6
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private System.DateTime bday;
+
+        Person(string name, string surname, DateTime bday)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.bday = bday;
+        }
+
+        Person()
+        {
+            this.name = "Ivan";
+            this.surname = "Ivanov";
+            this.bday = default;
+        }
+    }
+}
---
+++ Praktika 6/Praktika 6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Praktika_6
+{
+    enum Education
+    {
+        Bachelor,
+        Specialist,
+        SecondEducation
+    }
+
+    class Exam
+    {
+        public string nameLesson;
+        public int mark;
+        public DateTime date;
+
+        Exam(string nameLesson, int mark, DateTime date)
+        {
+            this.nameLesson = nameLesson;
+            this.mark = mark;
+            this.date = date;
+        }
+
+        Exam()
+        {
+            this.nameLesson = "Math";
+            this.mark = 5;
+            this.date = default;
+        }
+
+        public override string ToString()
+        {
+            return $"Название предмета: {nameLesson}, оценка: {mark}, дата проведения {date}";
+        }
+    }
+
+    class Student
+    {
+        private Person
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
