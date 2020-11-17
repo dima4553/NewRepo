a32509c0bfe6e68e0cc854ede8ddf513d5fd79b6
Дмитрий Пинчук <laik111@mail.ru>
2020-11-17 18:47:43 +0300

    fix & add pr 3 upr2

--- Praktika 3/02/Lesson1-Exercise1-CS/Program.cs
+++ Praktika 3/02/Lesson1-Exercise1-CS/Program.cs
@@ -20,7 +20,7 @@ namespace Lesson1_Exercise1_CS
                               "8005555535"};
             foreach (string s in input)
             {
-                if (IsPhone(s)) Console.WriteLine(ReformatPhone(s) + " is a phone number");
+                if (IsPhone(s)) Console.WriteLine(s + " is a phone number");
 
                 else if (IsZip(s)) Console.WriteLine(s + " is a zip code");
 
@@ -37,12 +37,6 @@ namespace Lesson1_Exercise1_CS
         static bool IsZip(string s)
         {
             return Regex.IsMatch(s, @"^\d{5}(\-\d{4})?$");
-        }
-
-        static string ReformatPhone(string s)
-        {
-            Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
-            return String.Format("({0}) {1}-{2}", m.Groups[1], m.Groups[2], m.Groups[3]);
         }
     }
 }
