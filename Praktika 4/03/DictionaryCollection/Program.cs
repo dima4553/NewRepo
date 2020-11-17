994039109dbf8ba3bcd579ac36ce31bfb0cc6082
Дмитрий Пинчук <laik111@mail.ru>
2020-11-17 20:02:04 +0300

    add pr4 pr3

---
+++ Praktika 4/03/DictionaryCollection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable["0"] = "Zero";
+            hashtable["1"] = "One";
+            hashtable["2"] = "Two";
+            hashtable["3"] = "Three";
+            hashtable["4"] = "Four";
+            hashtable["5"] = "Five";
+            hashtable["6"] = "Six";
+            hashtable["7"] = "Seven";
+            hashtable["8"] = "Eight";
+            hashtable["9"] = "Nine";
+
+            string ourNumber = "888-555-1212";
+
+            foreach (char c in ourNumber)
+            {
+                string digit = c.ToString();
+                if (hashtable.ContainsKey(digit))
+                {
+                    Console.WriteLine(hashtable[digit]);
+                }
+            }
+        }
+    }
+}
